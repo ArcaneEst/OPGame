3b6ff6fa5d6bf8a255d4b0e77ba4bdc9c69ef7bd
andrei-uni <optical_vizardless@aleeas.com>
2022-04-08 12:05:31 +0500

    init commit 2

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+     private BoxCollider2D boxCollider2D;
+     private Animator animator;
+     
+     [SerializeField] private float speed;
+     
+     private bool hit;
+
+     private void Awake()
+     {
+          boxCollider2D = GetComponent<BoxCollider2D>();
+          animator = GetComponent<Animator>();
+     }
+
+     private void Update()
+     {
+          if (hit) return;
+          var movementSpeed = speed * Time.deltaTime;
+          transform.Translate(0, -movementSpeed, 0); 
+     }
+
+     private void OnTriggerEnter2D(Collider2D col)
+     {
+          hit = true;
+          boxCollider2D.enabled = false;
+          animator.SetTrigger("explode");
+     }
+
+     public void Shoot()
+     {
+          gameObject.SetActive(true); 
+          hit = false;
+          boxCollider2D.enabled = true;
+     }
+
+     private void Deactivate()
+     {
+          gameObject.SetActive(false);
+     }
+}
