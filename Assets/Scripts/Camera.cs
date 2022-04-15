bd76d97b86770055f7b161f1a6050f6c3308e9ed
andrei-uni <optical_vizardless@aleeas.com>
2022-04-15 10:50:56 +0500

    Add Reload

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,13 +4,13 @@ public class Camera : MonoBehaviour
 {
     private GameObject player;
     private Vector3 offset;
+    
     private void Awake()
     {
         player = GameObject.FindWithTag("Player");
         offset = transform.position - player.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = player.transform.position + offset;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,33 +2,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body;
+    private Rigidbody2D player;
     private Animator animator;
     
-    private bool grounded;
-    private float beforeAttackCooldown = 0.5f;
-    private float cooldownTimerBeforeAttack = 0;
-
-    public float speed = 10;
-    public float jumpSpeed = 3;
-    public float attackRecoil = 3;
-
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject[] fireballs;
-    private float attackCooldown = 0.3f;
+
+    private const float Speed = 10;
+    private const float JumpHeight = 10;
+    private const float AttackRecoil = 3;
+    
+    private const float CooldownBetweenFireballs = 0.3f;
+    private const float CooldownBeforeAttack = 0.5f;
+    private const int MaxNumberOfFireballs = 4;
+    
+    private bool grounded;
+    private int currentNumberOfFireballs = 0;
+    private float cooldownTimerBeforeAttack = 0;
     private float cooldownTimerForAttack = Mathf.Infinity;
 
     private void Awake()
     {
-        body = GetComponent<Rigidbody2D>();
+        player = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        currentNumberOfFireballs = MaxNumberOfFireballs;
     }
 
     private void Update()
     { 
+        MovePlayer();
+        
+        if (Input.GetKey(KeyCode.Space))
+            SpacePressed();
+        
+        if (!grounded)
+            cooldownTimerBeforeAttack += Time.deltaTime;
+        
+        cooldownTimerForAttack += Time.deltaTime;
+        
+        animator.SetBool("grounded", grounded);
+    }
+
+    private void MovePlayer()
+    {
         var horizontal = Input.GetAxis("Horizontal");
         
-        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+        player.velocity = new Vector2(horizontal * Speed, player.velocity.y);
         
         // Flip player when moving to different directions horizontally
         if (horizontal > 0.01f)
@@ -36,15 +56,7 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontal < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
         
-        if (Input.GetKey(KeyCode.Space))
-            SpacePressed();
-        if (!grounded)
-            cooldownTimerBeforeAttack += Time.deltaTime;
-        
-        cooldownTimerForAttack += Time.deltaTime;
-        
         animator.SetBool("run", horizontal != 0);
-        animator.SetBool("grounded", grounded);
     }
 
     private void SpacePressed()
@@ -53,15 +65,18 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
-        else if (cooldownTimerBeforeAttack >= beforeAttackCooldown)
+        else if (cooldownTimerBeforeAttack >= CooldownBeforeAttack)
         {
-            Attack();
+            if (currentNumberOfFireballs > 0)
+            {
+                Attack();
+            }
         }
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        player.velocity = new Vector2(player.velocity.x, JumpHeight);
         grounded = false;
         
         animator.SetTrigger("jump");
@@ -69,31 +84,37 @@ public class PlayerMovement : MonoBehaviour
     
     private void Attack()
     {
-        if (cooldownTimerForAttack >= attackCooldown)
+        if (cooldownTimerForAttack >= CooldownBetweenFireballs)
         {
             cooldownTimerForAttack = 0;
-            var fireballIndex = FindFireball();
-            fireballs[fireballIndex].transform.position = firePoint.position;
-            fireballs[fireballIndex].GetComponent<Projectile>().Shoot();
-            body.velocity = new Vector2(body.velocity.x, attackRecoil);
-        }   
+            currentNumberOfFireballs -= 1;
+            SendFireball();
+            player.velocity = new Vector2(player.velocity.x, AttackRecoil);
+        }
     }
 
-    private int FindFireball()
+    private void SendFireball()
+    {
+        var index = FindAvailableFireballIndex();
+        fireballs[index].transform.position = firePoint.position;
+        fireballs[index].GetComponent<Projectile>().Shoot();
+    }
+    
+    private int FindAvailableFireballIndex()
     {
         for (var i = 0; i < fireballs.Length; i++)
             if (!fireballs[i].activeInHierarchy)
                 return i;
-
         return 0;
     }
-    
+
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Ground"))
         {
             cooldownTimerBeforeAttack = 0;
             grounded = true;
+            currentNumberOfFireballs = MaxNumberOfFireballs;
         }
     }
 }
