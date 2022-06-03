a7eee3028f359817c4e1fa0d7b6e6d80644f7f17
andrei-uni <92112053+andrei-uni@users.noreply.github.com>
2022-06-03 05:23:16 +0500

    Interface

---
+++ Assets/Scripts/UI/Instructions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Instructions : MonoBehaviour
+{
+    [SerializeField] private Image image;
+
+    private float timer = 0;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 7)
+            Destroy(gameObject);
+    }
+}
